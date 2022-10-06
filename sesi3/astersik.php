797d19d1015ac60b8f862221808f505e48da49a4
Andre Apriyana <47628631+dre03@users.noreply.github.com>
2022-10-06 21:24:00 +0700

    Tugas Jarvis sesi ketiga

---
+++ sesi3/astersik.php
@@ -0,0 +1,9 @@
+<?php
+echo "<h2>Soal no 2 membuat piramida bintang</h2>";
+echo "<br>";
+for($i=1;$i<=5;$i++){
+    for($j=1;$j<=$i;$j++){
+                echo "*";
+    }
+    echo "<br />";
+}
---
+++ sesi3/kelipatan3.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h2>Tugas 3 Looping Kelipatan 3</h2>";
+$bil = 3;
+while($bil<50){
+ echo "Bilangan : $bil <br/>";
+ $bil=$bil+3;
+}
